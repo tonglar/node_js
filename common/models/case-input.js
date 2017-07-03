33a07a4b171a1c686e3220e46ec17fadb651e431
tonglar <glarexe@gmail.com>
2017-07-03 17:14:28 +0700

    no message

---
+++ common/models/case-input.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(case_inputs) {
+    case_inputs.beforeRemote('create', function(context, user, next) {
+    context.args.data.date = Date.now();
+    context.args.data.publisherId = context.req.accessToken.userId;
+    next();
+  });
+};
